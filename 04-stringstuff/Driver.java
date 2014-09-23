320ff74fac480a977ee2e1232634dac9d52e63ea
Victor Tan <victortan18@gmail.com>
2014-09-22 21:21:23 -0400

    Homework

---
+++ 04-stringstuff/Driver.java
@@ -0,0 +1,27 @@
+/*
+mixstart
+
+  if (str.length()<3) return false;
+  String first = str.substring(1,3);
+  if (first.equals("ix")) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+*/
+
+/*
+makeOutWord
+
+  String first = out.substring(0,2);
+  String second = out.substring(2);
+  return first + word + second;
+*/
+
+/*
+firstHalf
+
+  int half = ((str.length())/2);
+  return str.substring(0,half);
+*/
