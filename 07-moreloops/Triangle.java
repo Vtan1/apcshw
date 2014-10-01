42604af25e6b8253248b25b876662c2bb39639ba
Victor Tan <victor.tan@cslab>
2014-10-01 08:42:50 -0400

    Classwork/Homework

---
+++ 07-moreloops/Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    public String forwardTri (int a) {
+	String ans = "";
+	int x = 0;
+	int y;
+	while (a >= x) {
+	    x = x + 1;
+	    while (x < a
+	}
+	return ans;
+    }
+}
