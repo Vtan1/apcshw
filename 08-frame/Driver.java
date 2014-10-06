88b4be5cb19a63c476fe64201f9cfe9b2501a6f6
Victor Tan <victortan18@gmail.com>
2014-10-05 20:16:01 -0400

    Homework

---
+++ 08-frame/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	Frame f = new Frame();
+	System.out.println(f.stringSplosion("Scorpion"));
+	System.out.println(f.stringX("xxx triple x rated"));
+	System.out.println(f.frame(5,4));
+    }
+}
