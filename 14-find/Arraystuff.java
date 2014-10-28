4d0f59dd4b50288918b8e4d916cb7bae298c45ac
Victor Tan <victor@gmail.com>
2014-10-28 08:41:52 -0400

    Classwork added

--- 14-find/Arraystuff.java
+++ 14-find/Arraystuff.java
@@ -56,4 +56,17 @@ public class Arraystuff {
 	}
 	return ans;
     }
+    public int sum67(int[] nums) {
+	int sum = 0;
+	for (int i = 0;i < nums.length;i++) {
+	    if (nums[i] == 6) {
+		while (nums[i] != 7) {
+		    i++;
+		}
+	    } else {
+		sum = sum + nums[i];
+	    }
+	}
+	return sum;
+    }
 }
