cf763b464646c62f474ae62b263d681ad83bc76e
Victor Tan <victor@gmail.com>
2014-11-10 08:42:26 -0500

    Test 3

---
+++ cat.java
@@ -0,0 +1,22 @@
+public class cat extends pet {
+    public Cat(String name) {
+	super(name);
+	//calls super's constructor with (name)
+    }
+
+    public String speak() {
+	return "meow";
+    }
+}
+
+public class loudDog extends pet {
+    public String speak() {
+	return super.speak() + " " + super.speak();
+    }
+}
+
+//Kennel
+
+for (int i = 0;i < petList.length;i++) {
+    System.out.println(petList[i].getName() + petList[i].speak())
+	}
