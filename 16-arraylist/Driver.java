a37318bd213408fb7b4a79cc07c2e9cc32ef8895
Victor Tan <victortan18@gmail.com>
2014-11-12 23:28:24 -0500

    Homework

--- 16-arraylist/Driver.java
+++ 16-arraylist/Driver.java
@@ -1,7 +1,33 @@
+import java.io.*;
+import java.util.*;
 public class Driver {
-    ArrayList<Integer> al = new ArrayList<Integer>();
-    Random r = new Random();
-    for (int i = 0; i < 20; i++) {
-	al.add(r.nextInt(5));
+    public static void main(String[] args) {
+	ArrayList<String> al;
+	al = new ArrayList<String>();
+	System.out.println(al);
+	al.add("hello");
+	System.out.println(al);
+
+	al.add("world");
+	System.out.println(al);
+
+	al.add("33");
+	System.out.println(al);
+	System.out.println(al.size());
+	System.out.println(al.get(1));
+
+	ArrayList<Integer> ai;
+	ai=new ArrayList<Integer>();
+	Random r = new Random();
+	for (int i = 0; i < 20; i++) {
+	    ai.add(r.nextInt(5));
+	}
+	System.out.println(ai);
+	for (int x = 0; x < ai.size() - 1; x++) {
+	    if (ai.get(x) == ai.get(x + 1)) {
+		ai.remove(x);
+	    }
+	}
+	System.out.println(ai);
     }
-    System.out.println(al);
+}
