32da11b7c4fb563d2b7e7a3f883ad7d63076717b
Victor Tan <victor@gmail.com>
2014-11-21 08:40:56 -0500

    Started diagonal

--- 18-wordsearch/WordSearch.java
+++ 18-wordsearch/WordSearch.java
@@ -33,7 +33,7 @@ public class WordSearch{
 	int r = row, c = col;
 	int orig = col;
 	boolean work = true;
-	if (c+w.length()<=board[r].length && r < board.length && c < board[r].length){
+	if (c+w.length()<=board[r].length && r < board.length && c < board[r].length && r > 0 && c > 0){
 	    for (int i=0;i<w.length();i++){
 		if (work) {
 		    if (board[r][c] == '.') {
@@ -181,15 +181,63 @@ public class WordSearch{
 	}
     }
 
+     public void addWordDDL(String w,int row, int col){
+	int r = row, c = col;
+	int orig = row;
+	int orig2 = col;
+	boolean work = true;
+	if (r-w.length()>=-1 && r > 0 && c > 0 && r+w.length()<=board.length && r < board.length && c < board[r].length){
+	    for (int i=0;i<w.length();i++){
+		if (work) {
+		    if (board[r][c] == '.') {
+			//board[r][c] = w.charAt(i);
+			r++;
+			c--;
+		    } else {
+			if (!(board[r][c] == w.charAt(i))) {
+			    System.out.println(w + " encountered a different word while going backwards diagonally.");
+			    work = false;
+			} else {
+			    r++;
+			    c--;
+			}
+		    }
+		}
+	    }
+	    r = orig;
+	    c = orig2;
+	    if (work) {
+		 for (int i=0;i<w.length();i++){
+		      if (board[r][c] == '.') {
+			board[r][c] = w.charAt(i);
+			r++;
+			c--;
+		      } else {
+			  if (board[r][c] == w.charAt(i)) {
+			      r++;
+			      c--;
+			  }
+		      }
+		 }
+	    }
+	} else {
+	    System.out.println(w + " is out of bounds.");
+	}
+    }
+
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	w.addWordHR("hello",3,25);
-	w.addWordHR("look",3,22);
-	w.addWordHL("look",10,3);
-     	w.addWordHL("look",3,5);
-	//w.addWordH("hello",100,5);
-	//w.addWordH("hello",30,555);
-	w.addWordVD("howdy",15,1);
-	System.out.println(w);
+	//try {
+	    w.addWordHR("hello",3,25);
+	    w.addWordHR("look",3,22);
+	    w.addWordHL("look",10,3);
+	    w.addWordHL("look",3,5);
+	    //w.addWordH("hello",100,5);
+	    w.addWordHR("hello",10,555);
+	    w.addWordVD("howdy",15,1);
+	    w.addWordDDL("swag",11,3);
+	    System.out.println(w);
+	    //} catch (Exception e) {
+	    //System.out.println("One of your parameters is out of bounds.");
     }
 }
