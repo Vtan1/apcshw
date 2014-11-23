09acb8f82cadc7893ee9129d4bc5f6bcbb75df01
Victor Tan <victortan18@gmail.com>
2014-11-23 15:43:14 -0500

    Wrap up

--- 18-wordsearch/WordSearch.java
+++ 18-wordsearch/WordSearch.java
@@ -33,7 +33,7 @@ public class WordSearch{
 	int r = row, c = col;
 	int orig = col;
 	boolean work = true;
-	if (c+w.length()<=board[r].length && r < board.length && c < board[r].length && r > 0 && c > 0){
+	if (c+w.length()<=board[0].length && r < board.length && c < board[0].length && r > 0 && c > 0){
 	    for (int i=0;i<w.length();i++){
 		if (work) {
 		    if (board[r][c] == '.') {
@@ -71,7 +71,7 @@ public class WordSearch{
 	int r = row, c = col;
 	int orig = col;
 	boolean work = true;
-	if (c-w.length()>=-1 && row > 0 && col > 0){
+	if (c-w.length()>=-1 && row >= 0 && col >= 0 && r < board.length && c < board[0].length){
 	    for (int i=0;i<w.length();i++){
 		if (work) {
 		    if (board[r][c] == '.') {
@@ -109,7 +109,7 @@ public class WordSearch{
 	int r = row, c = col;
 	int orig = row;
 	boolean work = true;
-	if (r+w.length()<=board.length && r < board.length && c < board[r].length){
+	if (r+w.length()<=board.length && r < board.length && c < board[0].length && row >= 0 && col >= 0){
 	    for (int i=0;i<w.length();i++){
 		if (work) {
 		    if (board[r][c] == '.') {
@@ -147,7 +147,7 @@ public class WordSearch{
 	int r = row, c = col;
 	int orig = row;
 	boolean work = true;
-	if (r-w.length()>=-1 && r > 0 && c > 0){
+	if (r-w.length()>=-1 && r >= 0 && c >= 0 && r < board.length && c < board[0].length){
 	    for (int i=0;i<w.length();i++){
 		if (work) {
 		    if (board[r][c] == '.') {
@@ -186,7 +186,7 @@ public class WordSearch{
 	int orig = row;
 	int orig2 = col;
 	boolean work = true;
-	if (r-w.length()>=-1 && r > 0 && c > 0 && r+w.length()<=board.length && r < board.length && c < board[r].length){
+	if (c-w.length()>=-1 && r >= 0 && c >= 0 && r+w.length()<=board.length && r < board.length && c < board[0].length){
 	    for (int i=0;i<w.length();i++){
 		if (work) {
 		    if (board[r][c] == '.') {
@@ -195,7 +195,7 @@ public class WordSearch{
 			c--;
 		    } else {
 			if (!(board[r][c] == w.charAt(i))) {
-			    System.out.println(w + " encountered a different word while going backwards diagonally.");
+			    System.out.println(w + " encountered a different word while going backwards and down diagonally.");
 			    work = false;
 			} else {
 			    r++;
@@ -225,19 +225,184 @@ public class WordSearch{
 	}
     }
 
+    public void addWordDDR(String w,int row, int col){
+	int r = row, c = col;
+	int orig = row;
+	int orig2 = col;
+	boolean work = true;
+	if (c+w.length()<=board[0].length && r >= 0 && c >= 0 && r+w.length()<=board.length && r < board.length && c < board[0].length){
+	    for (int i=0;i<w.length();i++){
+		if (work) {
+		    if (board[r][c] == '.') {
+			//board[r][c] = w.charAt(i);
+			r++;
+			c++;
+		    } else {
+			if (!(board[r][c] == w.charAt(i))) {
+			    System.out.println(w + " encountered a different word while going forwards and down diagonally.");
+			    work = false;
+			} else {
+			    r++;
+			    c++;
+			}
+		    }
+		}
+	    }
+	    r = orig;
+	    c = orig2;
+	    if (work) {
+		 for (int i=0;i<w.length();i++){
+		      if (board[r][c] == '.') {
+			board[r][c] = w.charAt(i);
+			r++;
+			c++;
+		      } else {
+			  if (board[r][c] == w.charAt(i)) {
+			      r++;
+			      c++;
+			  }
+		      }
+		 }
+	    }
+	} else {
+	    System.out.println(w + " is out of bounds.");
+	}
+    }
+
+    public void addWordDUL(String w,int row, int col){
+	int r = row, c = col;
+	int orig = row;
+	int orig2 = col;
+	boolean work = true;
+	if (c-w.length()>=-1 && r >= 0 && c >= 0 && r-w.length()>=-1 && r < board.length && c < board[0].length){
+	    for (int i=0;i<w.length();i++){
+		if (work) {
+		    if (board[r][c] == '.') {
+			//board[r][c] = w.charAt(i);
+			r--;
+			c--;
+		    } else {
+			if (!(board[r][c] == w.charAt(i))) {
+			    System.out.println(w + " encountered a different word while going backwards and up diagonally.");
+			    work = false;
+			} else {
+			    r--;
+			    c--;
+			}
+		    }
+		}
+	    }
+	    r = orig;
+	    c = orig2;
+	    if (work) {
+		 for (int i=0;i<w.length();i++){
+		      if (board[r][c] == '.') {
+			board[r][c] = w.charAt(i);
+			r--;
+			c--;
+		      } else {
+			  if (board[r][c] == w.charAt(i)) {
+			      r--;
+			      c--;
+			  }
+		      }
+		 }
+	    }
+	} else {
+	    System.out.println(w + " is out of bounds.");
+	}
+    }
+
+    public void addWordDUR(String w,int row, int col){
+	int r = row, c = col;
+	int orig = row;
+	int orig2 = col;
+	boolean work = true;
+	if (c+w.length()<=board[0].length && r >= 0 && c >= 0 && r-w.length()>=-1 && r < board.length && c < board[0].length){
+	    for (int i=0;i<w.length();i++){
+		if (work) {
+		    if (board[r][c] == '.') {
+			//board[r][c] = w.charAt(i);
+			r--;
+			c++;
+		    } else {
+			if (!(board[r][c] == w.charAt(i))) {
+			    System.out.println(w + " encountered a different word while going forwards and up diagonally.");
+			    work = false;
+			} else {
+			    r--;
+			    c++;
+			}
+		    }
+		}
+	    }
+	    r = orig;
+	    c = orig2;
+	    if (work) {
+		 for (int i=0;i<w.length();i++){
+		      if (board[r][c] == '.') {
+			board[r][c] = w.charAt(i);
+			r--;
+			c++;
+		      } else {
+			  if (board[r][c] == w.charAt(i)) {
+			      r--;
+			      c++;
+			  }
+		      }
+		 }
+	    }
+	} else {
+	    System.out.println(w + " is out of bounds.");
+	}
+    }
+
+    public void addWord(String w) {
+	Random R = new Random();
+	int RandomCall = R.nextInt(8);
+	Random r = new Random();
+	int randomc = r.nextInt(30);
+	int randomr = r.nextInt(20);
+	if (RandomCall == 0){
+	    addWordHR(w,randomr,randomc);
+	    //return true;
+	} else if (RandomCall == 1){
+	    addWordHL(w,randomr,randomc);
+	    //return true;
+	} else if (RandomCall == 2){
+	    addWordVD(w,randomr,randomc);
+	    //return true;
+	} else if (RandomCall == 3){
+	    addWordVU(w,randomr,randomc);
+	    //return true;
+	} else if (RandomCall == 4){
+	    addWordDDL(w,randomr,randomc);
+	    //return true;
+	} else if (RandomCall == 5){
+	    addWordDDR(w,randomr,randomc);
+	    //return true;
+	} else if (RandomCall == 6){
+	    addWordDUR(w,randomr,randomc);
+	    //return true;
+	} else {
+	    addWordDUL(w,randomr,randomc);
+	    //return true;
+	}
+    }
+    
+
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	//try {
-	    w.addWordHR("hello",3,25);
-	    w.addWordHR("look",3,22);
-	    w.addWordHL("look",10,3);
-	    w.addWordHL("look",3,5);
-	    //w.addWordH("hello",100,5);
-	    w.addWordHR("hello",10,555);
-	    w.addWordVD("howdy",15,1);
-	    w.addWordDDL("swag",11,3);
-	    System.out.println(w);
-	    //} catch (Exception e) {
-	    //System.out.println("One of your parameters is out of bounds.");
+	w.addWordHR("hello",1,25);
+	w.addWordHR("look",3,22);
+	w.addWordHL("look",1,20);
+	w.addWordHL("look",3,5);
+	w.addWordVD("howdy",0,18);
+	w.addWordDDL("swag",0,3);
+	w.addWordDDR("swoosh",14,24);
+	w.addWordDUL("tan",10,12);
+	w.addWordDUR("tan",10,10);
+	w.addWord("adding");
+	System.out.println(w);
     }
 }
