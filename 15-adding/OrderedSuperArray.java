344d1e3b3b0f5ed96967936ebcbc4561e4772846
Victor Tan <victor@gmail.com>
2014-11-26 08:40:28 -0500

    OrderedArray

---
+++ 15-adding/OrderedSuperArray.java
@@ -0,0 +1,7 @@
+public class OrderedSuperArray extends Sarray {
+    public boolean add(String s) {
+	String[] ans = new String[data.length + 1];
+	for (int x = 0; x < data.length;x++) {
+	    String z = Math.max(something,something);
+	    for (int y = 0; y < data[x].length();y++) {
+		if (
